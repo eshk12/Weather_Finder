f2ae47fa9facfb5b0f4ae008908ab33251e66e10
eshk12 <itshakbar@gmail.com>
2020-08-13 23:43:29 +0300

    update index title

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Index page',
+        title: 'Weather',
         name: 'Itshak Barabie'
     })
 })
