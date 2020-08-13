1afe39a235ce0adbceb6aa6f5312d07e133d05b1
eshk12 <itshakbar@gmail.com>
2020-08-13 22:54:46 +0300

    init commit

---
+++ src/app.js
@@ -0,0 +1,82 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const forecast = require('./utils/forecast')
+const geoCode = require('./utils/GeoCode')
+const app = express()
+
+//Define path for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Index page',
+        name: 'Itshak Barabie'
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about', { //this is the hbs file
+        title: 'About me',
+        name: 'Itshak Barabie'
+    })
+})
+app.get('/help', (req, res) => {
+    res.render('help', { //this is the hbs file
+        title: 'Help page',
+        content: 'Lorem ipsum',
+        name: 'Itshak Barabie'
+    })
+})
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'missing address'
+        })
+    }
+    //res.send('else')
+    geoCode(req.query.address, (error, {latitude, longtitude, location} = {}) => {
+        if (error) {
+            return res.send({error})
+        }
+        forecast(latitude, longtitude, (error, forecastData) => {
+            if (error) {
+                return res.send({error})
+            }
+            res.send({
+                forecase: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Itshak Barabie',
+        errorMessage: 'Help article not found.'
+    })
+})
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Itshak Barabie',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('postman-request')
+
+const forecast = (lat,lon,callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=97f360ab35f9213fa7794acbabd4140a&query='+lon+','+lat
+    request({url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect the server.', undefined)
+        } else if (body.error) {
+            callback('Invalid area.',undefined)
+        } else {
+            const data = body.current
+            callback(undefined, "It is currently " + data.temperature + " degrees out. It feels like " + data.feelslike + " degress out.")
+        }
+    })
+}
+module.exports = forecast
